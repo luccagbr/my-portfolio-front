9dc5be7b59680b7c1abee907aca92eba9d6c11b9
lucca-hybriun <lucca.vital@hybriun.com.br>
2026-05-04 19:49:10 -0300

    feat: Inicio da aplicação

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,9 @@ const nextConfig: NextConfig = {
 
         return config;
     },
+    compiler: {
+        styledComponents: true,
+    },
 };
 
 export default nextConfig;
---
+++ src/lib/StyledComponentsRegistry.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useServerInsertedHTML } from "next/navigation";
+import React, { useState } from "react";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+
+interface IStyledComponentsRegistry {
+    children: React.ReactNode;
+}
+
+export default function StyledComponentsRegistry({ children }: IStyledComponentsRegistry) {
+    const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+    useServerInsertedHTML(() => {
+        const styles = styledComponentsStyleSheet.getStyleElement();
+        styledComponentsStyleSheet.instance.clearTag();
+
+        return <>{styles}</>;
+    });
+
+    if (typeof window !== "undefined") return <>{children}</>;
+
+    return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
+}
